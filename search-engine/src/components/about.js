e2760bd301ceb1e85793b19b006c0e46b8cbcbba
DINESH <dineshsubramani0@gmail.com>
2021-12-29 12:41:37 +0530

    first commit

---
+++ search-engine/src/components/about.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const about = () => {
+  return(
+    <div className='ui raised very padded text container segment' style={{marignTop:'80px'}}>
+    <h1>About us </h1>
+    <p>My name is   Dinesh S .simply to be the best full stack developer that
+       I can be and contribute to the technology industry all that I know and can do.
+        I am dedicated to perfecting my craft by learning from more seasoned developers, 
+        remaining humble, and continuously making strides to learn all that I can about 
+        development.</p>
+</div>
+  )
+}
+
+export default about;
---
+++ search-engine/src/components/contact.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const contact = () => {
+  return(
+    <div className='ui raised very padded text container segment' style={{marignTop:'80px'}}>
+    <h1>Contact us </h1>
+    <ol>
+      <li>Email: dineshsubramani0@gmail.com</li>
+      <li>Email: dineshsubramanian0122@gmail.com</li>
+      <li>Mobile no : 9789504649</li>
+      <li>State:Tamil Nadu </li>
+      <li>City : Coimbatore</li>
+      <li>Linkedin: www.linkedin.com/in/dinesh-s-b896b6226</li>
+      <li>Github:github.com/DINESH0122</li>
+
+    </ol>
+</div>
+  )
+}
+
+export default contact;
